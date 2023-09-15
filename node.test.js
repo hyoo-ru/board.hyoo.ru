c6bdda6af227b675c9dbde9f56e527d8ee1aeecf
nin-jin <nin-jin@users.noreply.github.com>
2023-09-15 07:14:58 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -3054,11 +3054,8 @@ var $;
     var $$;
     (function ($$) {
         class $mol_ghost extends $.$mol_ghost {
-            dom_node(next) {
-                const node = this.Sub().dom_node(next);
-                $mol_dom_render_attributes(node, this.attr_static());
-                $mol_dom_render_events(node, this.event());
-                return node;
+            dom_node_external(next) {
+                return this.Sub().dom_node(next);
             }
             dom_node_actual() {
                 this.dom_node();
@@ -3093,9 +3090,6 @@ var $;
                 return this.Sub().minimal_height();
             }
         }
-        __decorate([
-            $mol_mem
-        ], $mol_ghost.prototype, "dom_node", null);
         __decorate([
             $mol_mem
         ], $mol_ghost.prototype, "dom_node_actual", null);
