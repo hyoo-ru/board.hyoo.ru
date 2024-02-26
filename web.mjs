4825ca16dfdef904fa4ce5584f42d4765c32d009
nin-jin <nin-jin@users.noreply.github.com>
2024-02-26 07:16:57 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.mjs
+++ web.mjs
@@ -3865,9 +3865,13 @@ var $;
                 return exists;
             if (next === exists)
                 return exists;
-            if (next)
+            if (next) {
                 this.parent().exists(true);
-            this.ensure();
+                this.ensure();
+            }
+            else {
+                this.drop();
+            }
             this.reset();
             return next;
         }
@@ -4156,6 +4160,9 @@ var $;
         }
         ensure() {
             throw new Error('$mol_file_web.ensure() not implemented');
+        }
+        drop() {
+            throw new Error('$mol_file_web.drop() not implemented');
         }
         sub() {
             throw new Error('$mol_file_web.sub() not implemented');
